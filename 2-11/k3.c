1907ae3c80b2039be4e08c8bb57be8740fd82832
Hony Sher <honysher@gmail.com>
2022-02-11 17:07:10 +0800

    detail Linear Sieve Method for Prime Numbers

--- 2-11/k3.c
+++ 2-11/k3.c
@@ -3,7 +3,6 @@
 #define MAXN 10000005
 
 int prime[MAXN];
-int pp[MAXN];
 int vis[MAXN];
 int is_huiwen(int x) {
 
@@ -23,17 +22,23 @@ int main() {
     int cnt = 0;
     if (b > 10000000)
         b = 10000000;
-    for (int i = 3; i <= b; i += 2) {
-        if (!vis[i])
-            prime[cnt++] = i, pp[i] = 1;
+    // 线性筛法
+    for (int i = 2; i <= b; i++) {
+        if (!vis[i])          // 没被筛到，说明是质数
+            prime[cnt++] = i; // 保存该质数
+        // 合数 = 最小质因数 * 最大因子
+        // 最需要枚举最小质因数
         for (int j = 0; j < cnt && i * prime[j] <= b; j++) {
             vis[i * prime[j]] = 1;
+            // 当 i % prime[j] == 0，不需要往后枚举，因为之后的不是最小质因数
+            // 如 4 % 2 == 0，不需要枚举 4 * 3，因为 4 * 3 = 2 * 6,
+            // 2才是最小质因
             if (i % prime[j] == 0)
                 break;
         }
     }
     for (int i = a; i <= b; i++) {
-        if (pp[i] && is_huiwen(i))
+        if (!vis[i] && is_huiwen(i))
             printf("%d\n", i);
     }
 }
