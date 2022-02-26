49155fbff6025681f0111430dd463cc71796eb2c
Hony Sher <honysher@gmail.com>
2022-02-26 15:16:19 +0800

    fix

--- Category/10_Recursion/f_1990.c
+++ Category/10_Recursion/f_1990.c
@@ -4,8 +4,6 @@ int f[1000001];
 int g[1000001];
 int mod = 10000;
 
-int mod_add(int a, int b) { return (a + b) % mod; }
-
 // 主要在于如何找状态转移方程
 int main(int argc, const char *argv[]) {
     int n;
@@ -13,8 +11,10 @@ int main(int argc, const char *argv[]) {
     f[0] = 1;
     g[1] = f[1] = 1;
     for (int i = 2; i <= n; i++) {
-        f[i] = mod_add(mod_add(f[i - 1], f[i - 2]), 2 * g[i - 2]);
-        g[i] = mod_add(f[i - 1], g[i - 1]);
+        f[i] = f[i - 1] + f[i - 2] + 2 * g[i - 2];
+        f[i] %= mod;
+        g[i] = f[i - 1] + g[i - 1];
+        g[i] %= mod;
     }
     printf("%d\n", f[n]);
     return 0;
